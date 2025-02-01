60aa3ab308670628c3d040e4c614541b76c5ed8c
NA29 <nathan.audegond@hotmail.com>
2025-02-01 12:30:42 -0500

    adding XCode files:

---
+++ DealifyApp/DealifyApp/DealifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealifyAppApp.swift
+//  DealifyApp
+//
+//  Created by Nathan Audegond on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DealifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DealifyApp/DealifyAppTests/DealifyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DealifyAppTests.swift
+//  DealifyAppTests
+//
+//  Created by Nathan Audegond on 01/02/2025.
+//
+
+import Testing
+@testable import DealifyApp
+
+struct DealifyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
