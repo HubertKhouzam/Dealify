0b6ed02581208bb351ca4c9cc94d59edb553e994
Hubert Khouzam <hub.khouzam@gmail.com>
2025-02-02 04:51:00 -0500

    propagation changement schema search

--- server/src/Dto/flask.dto.ts
+++ server/src/Dto/flask.dto.ts
@@ -7,6 +7,8 @@ export interface FlaskResponseImage {
 interface SearchResult {
   rank: number;
   text: string;
+  price: string;
+  store: string;
 }
 
 export type SearchResponse = SearchResult[];
